5729694ba54c47fd89607a401f648b02e510909e
harshil8980 <81788679+harshil8980@users.noreply.github.com>
2023-12-08 22:09:09 -0500

    Add files via upload

---
+++ Global_files/globalfooter.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style/style.css">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        footer {
+            margin-top: auto;
+            background-color:#000;
+            color:#fff;
+        }
+    </style>
+</head>
+<body>
+    <!-- Your main content goes here -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <footer class="mt-4 text-center">
+        <!-- Add your footer content here -->
+        &copy; <?php echo date("Y"); ?> PHP main project created by |Harshil Patel (200552678)|Mishant Dalwadi(200555960)
+    </footer>
+</body>
+</html>
---
+++ Global_files/globalheader.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <link rel="stylesheet" href="Style/style.css"> 
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <!-- Your Logo/Brand -->
+            <a class="navbar-brand" href="#">
+                <img src="Images\logo.jpg" alt="Your Logo" width="70" height="70" class="d-inline-block align-top rounded-circle">
+                <span class="ml-2 mt-4 ">PHP CRUD/WEBCONTAINT</span>
+            </a>
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Register.php">Registration</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
